c1dfbe36ba1cd8bd602da33c8d1ac8b2d5b96c4c
Dylan Tettarasar <dtettarasar@gmail.com>
2025-09-17 09:40:07 +0000

    update url img field setting in listing model

--- auctions/models.py
+++ auctions/models.py
@@ -24,7 +24,7 @@ class Listing(models.Model):
     description = models.TextField()
     start_bid = models.FloatField()
     current_bid = models.FloatField()
-    img_url = models.URLField(max_length=500)
+    img_url = models.URLField(max_length=500, null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="open")
 
     # for the creator and winner : if we have to delete an user, we keep the related listing, we'll set the listing creator to null
