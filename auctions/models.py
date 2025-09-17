cda6241a126a448b04760a7ee3a41b32ee80f6f3
Dylan Tettarasar <dtettarasar@gmail.com>
2025-09-17 09:29:25 +0000

    building the model for the Listing

--- auctions/models.py
+++ auctions/models.py
@@ -11,3 +11,23 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.cat_name}"
+    
+class Listing(models.Model):
+
+    STATUS_CHOICES = [
+        ("open", "Open"),
+        ("closed", "Closed"),
+        ("cancelled", "Cancelled"),
+    ]
+
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    start_bid = models.FloatField()
+    current_bid = models.FloatField()
+    img_url = models.URLField(max_length=500)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="open")
+
+    # for the creator and winner : if we have to delete an user, we keep the related listing, we'll set the listing creator to null
+    # make sure they have a related name so that 2 listing_sets can be created (as we have here to attributes related to the User model)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL,null=True,blank=True, related_name="created_listings")
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL,null=True,blank=True, related_name="won_listings")
