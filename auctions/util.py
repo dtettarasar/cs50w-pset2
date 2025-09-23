49319bd1a57eca90958949bf79eb85f4aadbd89b
Dylan Tettarasar <dtettarasar@gmail.com>
2025-09-23 12:07:34 +0200

    save listing function: check start bid value

--- auctions/util.py
+++ auctions/util.py
@@ -7,7 +7,8 @@ def save_listing(creator_user_id, l_title, l_description, l_start_bid, l_img_url
         'creator': creator_user_id,
         'title': l_title,
         'description': l_description,
-        'start_bid': l_start_bid,
+        'start_bid_str': l_start_bid,
+        'start_bid_float': None,
         'img_url': l_img_url,
         'category': l_category,
         'creation_status': None,
@@ -23,8 +24,20 @@ def save_listing(creator_user_id, l_title, l_description, l_start_bid, l_img_url
         
         listing_data['error_msg'].append("error: no description")
         
-    if listing_data["start_bid"] == '':
+    if listing_data["start_bid_str"] == '':
         
         listing_data['error_msg'].append("error: no starting bid")
+        
+    else:
+        
+        try:
+            
+            listing_data['start_bid_float'] = float(listing_data['start_bid_str'])
+            
+        except ValueError:
+            
+            listing_data['error_msg'].append("error: starting bid is not valid")
+        
+    
      
     return listing_data
